961724a63e659bd6792c29704c8dc3e189164abc
jangdayeon <dayeonjang.dev@gmail.com>
2024-10-15 20:05:57 +0900

    로그인 localStorage 연결 성공

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
-import { PropsWithChildren } from 'react';
-import { auth } from '@/lib/auth';
 import Button from '@/components/atoms/Button';
 
-export default async function Home({ children }: PropsWithChildren) {
-  console.log('🚀 ~ Home ~ children:', children);
-  const session = await auth();
-  console.log('🚀 ~ Home ~ session:', session);
+export default function Home() {
+  const session = useSession().data?.user?.email ?? '';
+  localStorage.setItem('user', session);
 
   return (
     <>
-      {session?.user ? (
+      {session ? (
         <div className='flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:w-10/12 md:w-8/12 lg:w-2/5 p-6 shadow shadow-blue-400 items-center gap-3'>
-          {session.user.name}님 안녕하세요!
+          {session}님 안녕하세요!
           <Link href='/recipes'>
             <Button variant='btn-primary'>레시피 확인하기</Button>
           </Link>
